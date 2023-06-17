7af099a6898ea6dfcf7c26a6f1aa0ac4f73e7c68
Miurel <miurelsoza16@gmail.com>
2023-06-17 14:55:02 -0700

    se agrega solucion

---
+++ FakeData/FakeData/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using FakeData.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FakeData.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
